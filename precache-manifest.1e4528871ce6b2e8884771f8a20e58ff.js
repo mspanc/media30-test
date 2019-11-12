b3be627bf03863463ea32de7ab8a094a474bbc5d
Marcin Lewandowski <marcin@saepia.net>
2019-11-12 19:36:21 +0100

    initial commit

---
+++ precache-manifest.1e4528871ce6b2e8884771f8a20e58ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7beba671aa293c34912f17f38b3756a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84f75721d83ae605479",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e7fe93dcf2ded27ee41e",
+    "url": "/static/js/2.8e6d623e.chunk.js"
+  },
+  {
+    "revision": "c84f75721d83ae605479",
+    "url": "/static/js/main.5fbdb43b.chunk.js"
+  },
+  {
+    "revision": "f3f7ef584c4f580a9d8b",
+    "url": "/static/js/runtime-main.d824bfb0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
